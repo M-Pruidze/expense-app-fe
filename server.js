541a92e5e7238c526916c2e64a0247a4d8c1a0bb
M-Pruidze <m.pruidze.exceedteam@gmail.com>
2021-10-18 17:02:55 +0400

    added public folder

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-// const router = express.Router();
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/home.html'));
