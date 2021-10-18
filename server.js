278e00d715cd9a5b4a8b7aad2550d9435949bea3
M-Pruidze <91416903+M-Pruidze@users.noreply.github.com>
2021-10-18 16:55:54 +0400

    Update server.js

--- server.js
+++ server.js
@@ -6,3 +6,11 @@ const path = require('path');
 app.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/home.html'));
 });
+
+const port = process.env.PORT || "4000";
+
+app.listen(port, () => {
+  console.log(`listen on `, port )
+})
+
+module.exports = app;
